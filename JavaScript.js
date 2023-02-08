5502a5f87e81a59df1dd77627346dca62201a29a
Vuyisa <vuyisa.mamba@sintrex.com>
2023-02-08 14:21:17 +0200

    fixed Icon.

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,5 @@
 // scroll to top functionality
-const scrollUp = document.querySelector("#scroll-up");
+/*const scrollUp = document.querySelector("#scroll-up");
 
 scrollUp.addEventListener("click", () => {
   window.scrollTo({
@@ -9,7 +9,7 @@ scrollUp.addEventListener("click", () => {
   });
 });
 
-/ Nav hamburgerburger selections
+// Nav hamburgerburger selections
 
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
@@ -28,4 +28,31 @@ navLink.forEach((link) =>
   link.addEventListener("click", () => {
     ul.classList.remove("show");
   })
-);
+);*/
+// Create button
+const scrollUpButton = document.createElement("button");
+scrollUpButton.innerHTML = "Go to top";
+scrollUpButton.classList.add("scroll-up");
+scrollUpButton.style.display = "none";
+document.body.appendChild(scrollUpButton);
+
+// Show button when scrolling down
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollUpButton.style.display = "block";
+  } else {
+    scrollUpButton.style.display = "none";
+  }
+};
+
+// Scroll to top when button is clicked
+scrollUpButton.addEventListener("click", function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+
+$('.navbar-toggle').click(function() {
+  $('.navbar-nav').slideToggle();
+});
+
